d732f26854442e5c1e0d28c76bdcfc01854e9310
Danil <redviagressif@gmail.com>
2020-04-29 17:09:21 +0300

    initial commit

---
+++ React/react1/src/App.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout'
+
+class App extends Component {
+  render(){
+    return(
+      <Layout>
+        <div style={{width:400, border: '1px solid black'}}></div>
+
+
+
+      </Layout>
+
+    )
+  }
+}
+export default App;
